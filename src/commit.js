5fc8ccf4f7022d48a0ebcbba936c36d55f0d7e61
Greg Walker <mgwalker@gmail.com>
2022-10-03 23:46:45 -0500

    oop

--- src/commit.js
+++ src/commit.js
@@ -1,11 +1,13 @@
 const util = require("./util");
 
 (async () => {
+  const bracketUser = await util.getUser();
+
   await util.exec(
     `git config --local user.email "mgwalker@users.noreply.github.com"`
   );
   await util.exec(`git config --local user.name "bracketeer"`);
-  await util.exec(`git add ./brackets/${bracketUser}.json`);
+  await util.exec(`git add .`);
   await util.exec(`git commit -m "adding ${bracketUser}'s bracket"`);
   await util.exec(
     `git push "https://${util.gh.owner}:$GITHUB_TOKEN@github.com/${util.gh.owner}/${util.gh.repo}.git" HEAD:main`
